feae61eb558f68c73d09f48da21b08e5b5271362
GBTC408012UR <GBTC408012UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-15 11:36:41 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Role.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+public class Role {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    @Column(unique = true)
+    private String role;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private Collection<User> users;
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public Role() {
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Collection<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Collection<User> users) {
+        this.users = users;
+    }
+}
